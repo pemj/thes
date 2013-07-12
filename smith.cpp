dc7cf9501cad1e50a0bca579468bec2786e0586f
Peter McKay <petermckayak@gmail.com>
2013-07-12 14:21:11 -0700

    Separateed forge into fields and forge, fixed field mechanics, added operators.

--- smith.cpp
+++ smith.cpp
@@ -69,6 +69,7 @@ int main(int argc, char *argv[]){
     }
     lineNo++;
     if((!strncmp(line,"//", 2))&&(strlen(line)<2)){
+      free(&line);
       continue;//on blank line or comment
     }
     //else tokenize
